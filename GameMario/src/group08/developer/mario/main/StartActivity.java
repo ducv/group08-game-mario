ddae84b9c88219f217a2502865a0f013203af4cf
nguyenduy.evillong@gmail.com <nguyenduy.evillong@gmail.com@cc1c5fa5-8eb1-245e-e3dd-6488e8a976f0>
2013-05-17 07:48:20 +0000

    Update V 1.01

---
+++ GameMario/src/group08/developer/mario/main/StartActivity.java
@@ -0,0 +1,53 @@
+package group08.developer.mario.main;
+
+import group08.developer.mario.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class StartActivity extends Activity implements OnClickListener {
+
+	private Button mainMenu;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.activity_start);
+		mainMenu = (Button) findViewById(R.id.mainmenu);
+		mainMenu.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+//		switch (v.getId()) {
+//		case R.id.mainmenu:
+//			Intent main = new Intent(getApplicationContext(),
+//					MainActivity.class);
+//			startActivity(main);
+//			break;
+//
+//		default:
+//			break;
+//		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.start, menu);
+		return true;
+	}
+
+}
+
