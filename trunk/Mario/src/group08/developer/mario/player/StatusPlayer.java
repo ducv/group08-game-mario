1ea174dd68beb37ded9b7f1ef1ca559e81d1fc27
nguyenduy.evillong@gmail.com <nguyenduy.evillong@gmail.com@cc1c5fa5-8eb1-245e-e3dd-6488e8a976f0>
2013-05-18 16:01:17 +0000

    Update mot so thu linh tinh

---
+++ trunk/Mario/src/group08/developer/mario/player/StatusPlayer.java
@@ -0,0 +1,10 @@
+package group08.developer.mario.player;
+
+/**
+ * @author EvilSakura
+ *
+ * @date May 18, 2013 7:42:42 PM
+ */
+public class StatusPlayer {
+
+}
---
+++ trunk/Mario/src/group08/developer/mario/sprite/InterfaceSprite.java
@@ -0,0 +1,10 @@
+package group08.developer.mario.sprite;
+
+/**
+ * @author EvilSakura
+ *
+ * @date May 18, 2013 7:43:32 PM
+ */
+public interface InterfaceSprite {
+
+}
